cec3204faf31e960446c1e149e3e14010b57995f
virajransing <virajransing@gmail.com>
2019-08-19 17:22:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,20 +1,19 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |key, value|
-    value.each do |key, value|
-      value.each do |new_value, names|
-        names.each do |name|
-          if !new_hash[name]
-            new_hash[name] = {}
-          end 
-          
-          if !new_hash[name][key]
-            new_hash[name][key] = []
-          end 
-          
-          new_hash[name][key] << new_value.to_s 
+    value.each do |new_value, names|
+      names.each do |name|
+        if !new_hash[name]
+          new_hash[name] = {}
         end 
+          
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end 
+          
+        new_hash[name][key] << new_value.to_s 
       end 
     end 
-    new_hash
-  end
+  end 
+  new_hash
+end
